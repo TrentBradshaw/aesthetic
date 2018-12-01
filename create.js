caf2ee395b90ab46fa70e8e1e4f72dfbd43e8475
TrentBradshaw <44883244+TrentBradshaw@users.noreply.github.com>
2018-11-30 18:03:00 -0800

    Add files via upload

--- create.js
+++ create.js
@@ -1,4 +1,13 @@
 const createProject = document.getElementById('createProject');
-createProject.addEventListener('click', function(e) {
-    window.location.href = ('http://127.0.0.1:8080/aesthetic.html')
-});
+
+    createProject.addEventListener('click', e => {
+        let projectName = document.getElementById('projectName');
+        const userRef = db.collection('users').doc(user.uid);
+        userRef.update({
+            aesthetic2: projectName.value
+        })
+        location.href = 'http://127.0.0.1:8080/profile.html';
+    })
+
+
+
